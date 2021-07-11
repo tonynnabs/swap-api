576304230396388bfea599e31026f2ca149d5b3b
tonynnabs <tonynnabs01@gmail.com>
2021-07-10 21:38:33 -0400

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                    <a href="{{ url('/people') }}" class="text-sm  rounded font-bold  bg-blue-500 px-3 text-white py-2">Dashboard</a>
                 @else
                     <a href="{{ route('login') }}" class="text-sm rounded font-bold bg-blue-500 px-3 text-white py-2">Log in</a>
 
